3f0840f00953673a5e3e3a0479ccb125375e2f27
teddy <teddyramantswana@gmail.com>
2023-06-15 10:46:21 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h> 
 
+/**
+*A programme that print a line followed by a new line
+*Your program should end with the value 0
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
