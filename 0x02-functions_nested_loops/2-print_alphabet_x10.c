e251d564fcc6dfe1fcb40eaf4d5290522c5eed16
teddy <teddyramantswana@gmail.com>
2023-06-20 15:15:37 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char x;
+
+	int y;
+
+	y = 0;
+
+	while (y < 10)
+	{
+	for (x = 'a'; x <= 'z';	x++)
+	{
+	_putchar(x);
+	}
+	_putchar('\n');
+	y++;
+	}
+}
+
