63d91f3e8e05be69e700563a9556fc59e7692545
teddy <teddyramantswana@gmail.com>
2023-06-29 12:50:54 -0700

    function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
 /**
  * _strcat - function that concatenates two strings
  * @dest: input value
- * @src: iput value
+ * @src: input value
  * Return: void
  */
 
@@ -13,7 +13,7 @@ int i;
 int j;
 
 i = 0;
-while (dest[1] != '\0')
+while (dest[i] != '\0')
 {
 i++;
 }
