73fbd6fa583c394967988938a26c595240e5efea
teddy <teddyramantswana@gmail.com>
2023-07-04 09:23:39 -0700

    function that copies memory area

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area
+ * @s: input
+ * @c: input
+ * Return: 0 (success)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i = 0;
+for (; s[i] >= '\0'; i++)
+{
+if (s[i] == c )
+return (&s[i]);
+}
+return (0);
+}
