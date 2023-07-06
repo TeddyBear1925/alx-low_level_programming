735d68f7044bec297ea27d23494eae04dbd0dd19
teddy <teddyramantswana@gmail.com>
2023-07-06 12:58:25 -0700

    returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - entry point
+ * @x: value
+ * @y: value
+ * Return: results
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+return (x * _pow_recrusion(x, y - 1));
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to be calculated
+ * Return: the results
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (actual_sqrt_recursi0n(n, 0));
+
