5d1a897af0ba6f4de3a5b30615bf75cf179a5de8
teddy <teddyramantswana@gmail.com>
2023-07-18 11:07:07 -0700

    Write a function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,5 +15,5 @@ if (d->name == NULL)
 d->name = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
-printf("name: %sf\nowner: %s\n", d->name, d->age, d->owner);
+printf("name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
