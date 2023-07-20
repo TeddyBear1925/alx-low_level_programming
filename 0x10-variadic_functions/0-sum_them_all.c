e3ad88e67464e2c7f6a25fe072068f514de7b306
teddy <teddyramantswana@gmail.com>
2023-07-20 12:32:12 -0700

    Write a function that returns the sum of all its parameters.

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,4 @@
-#include "variadic_function.h"
+#include "variadic_functions.h"
 #include <stdarg.h>
 
 /**
@@ -7,12 +7,12 @@
  * Return: f n == 0;
  */
 
-int sum_them_all(const unsinged int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
 unsigned int i, sum = 0;
 va_start(ap, n);
-for (i = 0; i < n; i++;
+for (i = 0; i < n; i++);
 sum += va_arg(ap, int);
 va_end(ap);
 return (sum);
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - printsnumbers followed by a new line
+ * @separator: the string to be printed
+ * @n: the number of intergerpassed to the function
+ * @....: a variable number
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list nums;
+unsigned int index;
+va_start(nums, n)
+for (index = 0; index < n; index++)
+{
+printf("%d", va_arg(nums, int));
+if (index != (n - 1) && separotor != NULL)
+printf("%s", separator);
+}
+printf("\n");
+va_end(nums);
+}
